1dd95b5fb371f324941f0fb28a2a73cadb6bf010
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2024-10-20 01:56:35 -0300

    ajuste stress test

--- src/stress-test/user-files/simulations/secomp/SecompSimulation.scala
+++ src/stress-test/user-files/simulations/secomp/SecompSimulation.scala
@@ -37,7 +37,7 @@ class SecompSimulation
 
   setUp(
     transferencias.inject(
-      rampUsersPerSec(1).to(1000).during(5.minutes)
+      rampUsersPerSec(1).to(2000).during(5.minutes)
     )
   ).protocols(httpProtocol)
 }
